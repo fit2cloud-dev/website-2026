6cd9cad15e3aa3e4d1835f8139975195fc2caddd
Zhimin Ruan <zhimin@fit2cloud.com>
2026-09-10 17:00:53 +0800

    优化 DE 加载速度

--- dataease/js/app.js
+++ dataease/js/app.js
@@ -73,7 +73,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 3000);
+        $('html, body').animate({ scrollTop: 0 }, 600);
         return false;
     });
 
